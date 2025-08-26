2fc2ee02b4480decf9d5f2edeb5a4b625106b4b7
ab10680 <10680@holbertonstudents.com>
2025-08-26 14:53:35 +0000

    task 6 - handleProfileSignup

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,8 +4,12 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  // signUpUser THEN uploadPhoto
   return Promise.allSettled([
-    uploadPhoto(fileName),
     signUpUser(firstName, lastName),
-  ]);
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((r) => (r.status === 'fulfilled'
+      ? { status: 'fulfilled', value: r.value }
+      : { status: 'rejected', value: (r.reason instanceof Error ? r.reason.toString() : r.reason) })));
 }
