79a8c137fbc1ae0f0a01fc9c6b5faadb8aaba5b1
ab10680 <10680@holbertonstudents.com>
2025-08-26 14:57:35 +0000

    task 9 - guardrail

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+// 9-try.js
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
