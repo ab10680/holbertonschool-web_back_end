5d1f92d0a8d8132fffbf1f5e774ae7d7786ea26c
ab10680 <10680@holbertonstudents.com>
2025-08-26 14:29:24 +0000

    task 4 - updateStudentGradeByCity

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+// 4-update_grade_by_city.js
+
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((g) => g.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
