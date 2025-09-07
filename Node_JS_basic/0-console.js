6fd2031d214afe92a4cee9ce61876d0083b86c43
ab10680 <10680@holbertonstudents.com>
2025-09-07 15:43:30 +0000

    task 0 - console message

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,12 +1,5 @@
-/** 
- * 0. Executing basic javascript with Node JS
- *
- * In the file 0-console.js, create a function named
- * displayMessage that prints in STDOUT the string argument.
- */
-
+/* eslint-disable no-console */
 function displayMessage(message) {
-  // print message to STDOUT
   console.log(message);
 }
 
